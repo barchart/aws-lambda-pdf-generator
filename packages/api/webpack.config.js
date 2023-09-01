07687d47bc8ec01660160d99c4edb6ba19eecf77
Igor Losev <igor.losev.it@gmail.com>
2023-09-01 13:27:07 +0200

    Suppressed webpack warnings related to dynamic dependency import

--- packages/api/webpack.config.js
+++ packages/api/webpack.config.js
@@ -7,14 +7,25 @@ module.exports = {
 	devtool: 'source-map',
 	resolve: {
 		mainFields: ['main', 'module'],
-		extensions: ['.wasm', '.js', '.mjs', '.json'],
+		extensions: ['.wasm', '.js', '.mjs', '.json']
 	},
 	plugins: [
 		new webpack.ContextReplacementPlugin(/\/log4js\/lib\/appenders/, (data) => {
 			delete data.dependencies[0].critical;
 
 			return data;
+		}),
+		new webpack.ContextReplacementPlugin(/\/yargs(-parser)?\/build/, (data) => {
+			data.dependencies.forEach((dependency) => {
+				delete dependency.critical;
+			});
+
+			return data;
 		})
 	],
-	externals: [ '@sparticuz/chromium' ]
+	externals: {
+		'@sparticuz/chromium': '@sparticuz/chromium',
+		'bufferutil': 'bufferutil',
+		'utf-8-validate': 'utf-8-validate'
+	}
 };
